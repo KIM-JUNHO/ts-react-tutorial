3c847abfa73c11fc9366891b40c813c49456778f
Unknown <prpr48@naver.com>
2020-01-23 00:49:02 +0900

    optional

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -3,12 +3,14 @@ import React from "react";
 type GreetingsProps = {
   name: string;
   mark: string;
+  optional?: string;
 };
 
-function Greetings({ name, mark }: GreetingsProps) {
+function Greetings({ name, mark, optional }: GreetingsProps) {
   return (
     <div>
       Hello, {name} {mark}
+      {optional && <p>{optional}</p>}
     </div>
   );
 }
